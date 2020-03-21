83ac0347f7d0aeb0e85cf294f8c463bbd5e95464
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-03-21 11:14:14 +0100

    arrays

--- src/app/arrays/arrays.component.ts
+++ src/app/arrays/arrays.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-arrays',
@@ -6,11 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./arrays.component.less']
 })
 export class ArraysComponent implements OnInit {
-  array: number[][]=[[1,2],[3,4],[1,2],[3,4]];
+  originalNumbers: number[][] = [[1, 2, 3, 4], [3, 4, 7, 5], [1, 2, 1, 1], [3, 4, 8, 5]];
+  array: number[][] = [[]];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
   }
 
+  ngOnInit(): void {
+    this.array = this.originalNumbers;
+    this.evaluateSquares();
+  }
+  evaluateSquares(): void {
+  }
+
+  f0(): void {
+    for (let i = 0; i < 10; i++) {
+      let element: number[] = [];
+      for (let j = 0; j < i; j++) {
+        element.push(j);
+      }
+      this.originalNumbers.push(element);
+      element = []
+    }
+  }
 }
