f42d1039e489314ef912dc054a175921384884a0
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-03-28 11:01:54 +0100

    cd ..

--- src/app/arrays/arrays.component.ts
+++ src/app/arrays/arrays.component.ts
@@ -10,6 +10,7 @@ export class ArraysComponent implements OnInit {
   array: number[][] = [[]];
   originalNumbers1D = [1,2,3,4,5,6,7,8,9,10];
   array1: number[] = [1,2,3,4,5,6,7,8,9,10];
+  array1Dinfo = '';
 
   constructor() {
   }
