29ea17e4e02ba401917dce7db4e9423b8ae6bbc8
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-04-04 11:12:32 +0200

    enums

--- src/app/array-filters/array-filters.component.ts
+++ src/app/array-filters/array-filters.component.ts
@@ -5,6 +5,7 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './array-filters.component.html',
   styleUrls: ['./array-filters.component.less']
 })
+
 export class ArrayFiltersComponent implements OnInit {
   array1: number[] = [1,2,3,4,5,6,7,8,9,10];
   originalNumbers1D: number[] = [1,2,3,4,5,6,7,8,9,10];
