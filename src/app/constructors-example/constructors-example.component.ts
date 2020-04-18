9df36a3c2c43d070d0da30d9ad3012b7115f1ed9
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-04-18 10:33:30 +0200

    cd..

--- src/app/constructors-example/constructors-example.component.ts
+++ src/app/constructors-example/constructors-example.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConstructorsExampleComponent implements OnInit {
 
+  array1: number[] = [1,2,3,4,5,6,7,8,9,10];
+  originalNumbers1D: number[] = [1,2,3,4,5,6,7,8,9,10];
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  originalArray1D() {
+  }
 }
