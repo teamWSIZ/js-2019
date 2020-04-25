110407a17289eed5513064e988b1337877c43b30
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-04-25 11:30:13 +0200

    cd..

--- src/app/sort-algoritms/sort-algoritms.component.ts
+++ src/app/sort-algoritms/sort-algoritms.component.ts
@@ -6,16 +6,15 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./sort-algoritms.component.less']
 })
 export class SortAlgoritmsComponent implements OnInit {
-  array: number[] = [1, 100, 2, 3, 40, 5, 6, 7, 8];
+  array: number[] = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
   sortSteps = [];
-  originalNumbers1D: number[] = [1, 100, 2, 3, 40, 5, 6, 7, 8];
+  originalNumbers1D: number[] = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
 
   constructor() {
   }
 
   ngOnInit(): void {
     console.log(this.array)
-    this.sortSteps.push(this.array)
     // this.array1.sort((n1,n2)=>n1-n2);
     // console.log(this.array1)
     // [1, 100, 2, 3, 4, 5, 6, 7, 8, 9, 10]
@@ -23,9 +22,10 @@ export class SortAlgoritmsComponent implements OnInit {
     // [1, 2, 3, 100, 4, 5, 6, 7, 8, 9, 10]
     // [1, 2, 3, 4, 100, 5, 6, 7, 8, 9, 10]
     let swap = true;
-    while(swap) {
-      swap=false;
-      for (let i = 0; i < this.array.length - 1; i++) {
+    let maxArrayLength = this.array.length - 1
+    while (swap) {
+      swap = false;
+      for (let i = 0; i < maxArrayLength; i++) {
         const n0 = this.array[i];
         const n1 = this.array[i + 1];
         if (n0 > n1) {
@@ -36,6 +36,7 @@ export class SortAlgoritmsComponent implements OnInit {
         console.log(this.array);
         this.sortSteps.push([...this.array]);
       }
+      maxArrayLength = maxArrayLength - 1;
       this.sortSteps.push('_________________');
     }
     console.log(this.sortSteps);
