b49b98d5f01994624124c85ab1cf9f07d5e6e182
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-04-25 11:10:34 +0200

    cd..

--- src/app/sort-algoritms/sort-algoritms.component.ts
+++ src/app/sort-algoritms/sort-algoritms.component.ts
@@ -6,9 +6,9 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./sort-algoritms.component.less']
 })
 export class SortAlgoritmsComponent implements OnInit {
-  array: number[] = [1, 100, 2, 3, 40, 5, 6, 7, 8, 9, 10];
-  sortSteps = [[]];
-  originalNumbers1D: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  array: number[] = [1, 100, 2, 3, 40, 5, 6, 7, 8];
+  sortSteps = [];
+  originalNumbers1D: number[] = [1, 100, 2, 3, 40, 5, 6, 7, 8];
 
   constructor() {
   }
@@ -22,16 +22,19 @@ export class SortAlgoritmsComponent implements OnInit {
     // [1, 2, 100, 3, 4, 5, 6, 7, 8, 9, 10]
     // [1, 2, 3, 100, 4, 5, 6, 7, 8, 9, 10]
     // [1, 2, 3, 4, 100, 5, 6, 7, 8, 9, 10]
+    let isSorted = true;
     for(let i=0; i<this.array.length-1; i++){
       const n0=this.array[i];
       const n1=this.array[i+1];
       if(n0>n1){
         this.array[i]=n1;
         this.array[i+1]=n0;
+        isSorted=false;
       }
       console.log(this.array);
       this.sortSteps.push(this.array);
     }
+    console.log(this.sortSteps);
   }
 
 }
