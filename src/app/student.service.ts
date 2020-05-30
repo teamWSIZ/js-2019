e71ea384167002185603d9237782cd8dfe7e2bb3
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-05-30 10:28:05 +0200

    cd..

--- src/app/student.service.ts
+++ src/app/student.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import {Student} from './student';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StudentService {
 
-  constructor() { }
+  students:Student[] = [{name: 'Przemysław', surname: 'Stokłosa', height: 174}]
+
+  constructor() {
+    for(let i=0;i<10;i++){
+      this.students.push({name: 'Przemysław', surname: 'Stokłosa'+i, height: 165+i})
+    }
+    this.students.push({name: 'Anna', surname: 'Łęcka',height: 200})
+    this.students.push({name: 'Anna', surname: 'Lęcka', height: 201})
+  }
+  getStudents():Student[]{
+    return this.students;
+  }
 }
