9d9315959567be47d180e8c9f1b0541db00b52e0
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-06-13 10:25:30 +0200

    cd..

--- src/app/temperature/temperature.component.ts
+++ src/app/temperature/temperature.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {SensorService} from '../sensor.service';
 import {SensorData} from '../SensorData';
+import {Label} from 'ng2-charts';
 
 @Component({
   selector: 'app-temperature',
@@ -15,6 +16,7 @@ export class TemperatureComponent implements OnInit {
     {data: [], label: 'temperatura'}
     // {data: [2,3,3], label: 'y'},
   ];
+  chartLabel: Label[] = []
 
   constructor(private sensorService: SensorService) {
     // this.sensorData = sensorService.getSensorData()
@@ -22,6 +24,7 @@ export class TemperatureComponent implements OnInit {
       this.sensorData=data
       data.forEach(element=>{
         this.chartData[0].data.push(Number(element.temp))
+        this.chartLabel.push(''+Number(element.temp))
         console.log(`element: ${element.temp}`)
       })
     })
