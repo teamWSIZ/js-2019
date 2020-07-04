4fcc80454c7f2121b3acf94915965d756eb107c5
Live session user <przemyslaw.stoklosa@gmail.com>
2020-07-04 08:52:20 +0000

    cd..

--- src/app/equations/equations.component.ts
+++ src/app/equations/equations.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {SensorData} from '../SensorData';
+import {Label} from 'ng2-charts';
 
 @Component({
   selector: 'app-equations',
@@ -6,6 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./equations.component.less']
 })
 export class EquationsComponent implements OnInit {
+  functionData : Number[] = []
+
+  chartOptions = {responsive: true};
+  chartDataLinearEquation = [
+    {data: [], label: 'linear equation'},
+    // {data: [2,3,3], label: 'y'},
+  ];
+
+  chartDataTemperatureType='line'
+  chartLabel: Label[] = []
+
 
   constructor() { }
 
