c4d37ab4bfe521a72e20c138d5cebef4b00e1416
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-09-19 11:02:51 +0200

    cd..

--- src/app/PlotData.ts
+++ src/app/PlotData.ts
@@ -1,3 +1,8 @@
+import {Observable, of} from 'rxjs';
+import {HttpClient} from '@angular/common/http';
+import {SensorData} from './SensorData';
+import {catchError} from 'rxjs/operators';
+
 export class PlotData{
     x: number;
     y: number;
--- src/app/plot-service.service.ts
+++ src/app/plot-service.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import {SensorData} from './SensorData';
+import {PlotData} from './PlotData';
+import {Observable, of} from 'rxjs';
+import {catchError} from 'rxjs/operators';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlotServiceService {
+  plotData: PlotData[] = []
+  url = "http://localhost:3000/chart_data"
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getPlotData():Observable<PlotData[]>{
+    const data = this.http.get<PlotData[]>(`${this.url}`).
+    pipe(catchError(err=>{
+      console.log(`Coś poszło nie tak...${err.message}`)
+      return of(this.plotData)
+    }))
+    return data
+  }
 }
